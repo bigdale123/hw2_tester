f475d66a0657d1d6e92a7830e11efab6776fcafe
bigdale123 <dylcal138@gmail.com>
2022-09-12 14:26:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ mkdir ~/.local/bin
 mkdir ~/.local/bin/hw2_tester
 
 # upload files to the hw2_tester
-wget <tester-source-url> --output-document ~/.local/bin/hw2_tester/TA_asgn2_testv2.c
-wget <Makefile-url> --output-document ~/.local/bin/hw2_tester/Makefile
-wget <tester-url> --output-document ~/.local/bin/hw2_tester/hw2_tester
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/TA_asgn2_testv2.c --output-document ~/.local/bin/hw2_tester/TA_asgn2_testv2.c
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/Makefile --output-document ~/.local/bin/hw2_tester/Makefile
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/hw2_tester --output-document ~/.local/bin/hw2_tester/hw2_tester
 
 # Update .bashrc file
-wget <bashrc-url> --output-document ~/.bashrc
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/bashrc --output-document ~/.bashrc
 
 # Apply bashrc changes
 source ~/.bashrc
