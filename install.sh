2ac1aa31ccf0286768d3455f593e69b94d8adbd4
bigdale123 <dylcal138@gmail.com>
2022-09-12 12:25:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Create binary folders
+mkdir ~/.local/bin
+mkdir ~/.local/bin/hw2_tester
+
+# upload files to the hw2_tester
+wget <tester-source-url> --output-document ~/.local/bin/hw2_tester/TA_asgn2_testv2.c
+wget <Makefile-url> --output-document ~/.local/bin/hw2_tester/Makefile
+wget <tester-url> --output-document ~/.local/bin/hw2_tester/hw2_tester
+
+# Update .bashrc file
+wget <bashrc-url> --output-document ~/.bashrc
+
+# Apply bashrc changes
+source ~/.bashrc
