a943f98d2472efbd8e4e8aabdc2b6c91dda7566d
bigdale123 <dylcal138@gmail.com>
2022-09-28 16:25:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Create binary folders
+mkdir ~/.local
 mkdir ~/.local/bin
 mkdir ~/.local/bin/hw2_tester
+mkdir ~/.local/bin/hw4_tester
 
 # upload files to the hw2_tester
 wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/TA_asgn2_testv2.c --output-document ~/.local/bin/hw2_tester/TA_asgn2_testv2.c
 wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/Makefile --output-document ~/.local/bin/hw2_tester/Makefile
 wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/hw2_tester --output-document ~/.local/bin/hw2_tester/hw2_tester
+
+# upload files to the hw4_tester
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/hw4_tester/Makefile --output-document ~/.local/bin/hw4_tester/Makefile
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/hw4_tester/TA_asgn4.c --output-document ~/.local/bin/hw4_tester/TA_asgn4.c
+wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/hw4_tester/hw4_tester --output-document ~/.local/bin/hw4_tester/hw4_tester
 
 # Update .bashrc file
 wget https://raw.githubusercontent.com/bigdale123/hw2_tester/main/bashrc --output-document ~/.bashrc
